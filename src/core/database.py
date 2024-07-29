ab8356a110afb4e1b9c08e8dffac10f2c3ac73da
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2024-07-29 19:54:45 +0500

    ✨ Database and migration settings are configured (#1)
    * deps: added database related dependencies
    
    * feat(config): added `DatabaseSettings` and general `Settings`
    
    * feat(database): settings for models and migrations creation
    
    * refactor(database): database settings related files are restructured
    
    * fix(database): correct db URL
    
    * feat(alembic): added filename template

---
+++ src/core/database.py
@@ -0,0 +1,17 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+from config import settings
+
+Base = declarative_base()
+metadata = Base.metadata
+
+engine = create_async_engine(settings.db.db_url)
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
