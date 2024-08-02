e049ba4167bedea4e170d01d38bf5d7a24d0599e
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2024-08-02 16:03:08 +0500

    refactor(auth): `LoginSchema` is removed

--- src/schemas/auth.py
+++ src/schemas/auth.py
@@ -3,11 +3,6 @@ from uuid import UUID
 from pydantic import BaseModel, EmailStr
 
 
-class LoginSchema(BaseModel):
-    email: EmailStr
-    password: str
-
-
 class TokenSchema(BaseModel):
     access_token: str
     token_type: str
