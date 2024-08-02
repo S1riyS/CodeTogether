67c6bc6bfdd771696948ce58c49c6b75a602545f
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2024-08-02 16:05:59 +0500

    feat(user): retrieve and update current user endpoints

--- src/routers/v1/user.py
+++ src/routers/v1/user.py
@@ -2,11 +2,23 @@ from uuid import UUID
 
 from fastapi import APIRouter
 
-from routers.depenencies import SessionDep
-from schemas.user import UserSchema
+from routers.depenencies import SessionDep, CurrentUserDep
+from schemas.user import UserSchema, UserUpdateSchema
 from services.user_service import UserService
 
 router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.get("/me", response_model=UserSchema)
+async def get_me(user: CurrentUserDep):
+    return UserSchema.from_orm(user)
+
+
+@router.put("/me", response_model=UserSchema)
+async def update_user(user: CurrentUserDep, data: UserUpdateSchema, session: SessionDep):
+    user_service = UserService(session)
+    db_obj = await user_service.update(user.id, data)
+    return UserSchema.from_orm(db_obj)
 
 
 @router.get("/{user_id}", response_model=UserSchema)
