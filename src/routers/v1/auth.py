32b3d1b90ec40ccceca3de38b90e92ff3241aaa4
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2024-08-03 17:44:10 +0500

    chore(auth): schema naming (changed to `SignUpSchema`)

--- src/routers/v1/auth.py
+++ src/routers/v1/auth.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
 from routers.depenencies import SessionDep
-from schemas.auth import TokenSchema, LoginSchema
-from schemas.user import UserSchema, UserCreateSchema as SignupSchema
+from schemas.auth import TokenSchema
+from schemas.user import UserSchema, UserCreateSchema as SignUpSchema
 from services.auth_service import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
@@ -16,7 +16,7 @@ async def login(session: SessionDep, data: OAuth2PasswordRequestForm = Depends()
 
 
 @router.post("/signup", response_model=UserSchema)
-async def signup(data: SignupSchema, session: SessionDep):
+async def signup(data: SignUpSchema, session: SessionDep):
     auth_service = AuthService(session)
     db_obj = await auth_service.signup(data)
     return UserSchema.from_orm(db_obj)
