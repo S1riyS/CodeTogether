3824e0a94cf8012fbc494eecdea675ea1663d7cb
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2024-08-15 22:21:23 +0500

    feat(models): added `UserPositionAssociation`

---
+++ src/models/associations/__init__.py
@@ -0,0 +1,1 @@
+from .user_position import UserPositionAssociation
---
+++ src/models/associations/user_position.py
@@ -0,0 +1,32 @@
+from typing import TYPE_CHECKING
+from uuid import UUID
+
+from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from core.database import Base
+from models.utils.mixins import TimeModelMixin
+
+if TYPE_CHECKING:
+    from models import UserModel, PositionModel
+
+
+class UserPositionAssociation(Base, TimeModelMixin):
+    __tablename__ = 'participations'
+    __table_args__ = (
+        UniqueConstraint(
+            "user_id",
+            "position_id",
+            name="idx_unique_user_position",
+        ),
+    )
+
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey('users.id'))
+    position_id: Mapped[UUID] = mapped_column(ForeignKey('positions.id'))
+    is_approved: Mapped[bool] = mapped_column(default=False)
+
+    # Association between Association -> User
+    user: Mapped["UserModel"] = relationship(back_populates="positions_details")
+    # Association between Association -> Position
+    position: Mapped["PositionModel"] = relationship(back_populates="users_details")
