db8b3b65bbc1bc666b74c14bf9be279774131e7e
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2024-08-15 14:18:24 +0500

    feat(position): Added `PositionModel`

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from core.database import Base
 from models.project import ProjectModel
+from models.position import PositionModel
 from models.user import UserModel
---
+++ src/models/position.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING, Optional
+from uuid import UUID
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from core.database import Base
+from models.utils.mixins import IDModelMixin
+
+if TYPE_CHECKING:
+    from models import ProjectModel
+
+
+class PositionModel(Base, IDModelMixin):
+    __tablename__ = "positions"
+
+    name: Mapped[str]
+    description: Mapped[Optional[str]]
+    count: Mapped[int] = mapped_column(default=1)
+    project_id: Mapped[UUID] = mapped_column(ForeignKey("projects.id", ondelete="CASCADE"))
+
+    project: Mapped["ProjectModel"] = relationship(back_populates="positions")
