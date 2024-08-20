6e411e1c9ba625698d44be9a144bc7423d43caab
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2024-08-20 17:06:41 +0500

    :sparkles: feat(position): endpoints for operations with positions

--- src/routers/v1/position.py
+++ src/routers/v1/position.py
@@ -2,17 +2,25 @@ from uuid import UUID
 
 from fastapi import APIRouter
 
+from routers.depenencies import CurrentUserDep, SessionDep
+from schemas.position import PositionUpdateSchema, PositionSchema
+from services.position_service import PositionService
+
 router = APIRouter(prefix="/positions", tags=["Positions"])
 
 
-@router.put("/{position_id}")
-async def update_position(position_id: UUID):
-    ...
+@router.put("/{position_id}", response_model=PositionSchema)
+async def update_position(position_id: UUID, data: PositionUpdateSchema, user: CurrentUserDep, session: SessionDep):
+    position_service = PositionService(session)
+    db_obj = await position_service.update(position_id, data, user.id)
+    return PositionSchema.from_orm(db_obj)
 
 
-@router.delete("/{position_id}")
-async def delete_position(position_id: UUID):
-    ...
+@router.delete("/{position_id}", response_model=bool)
+async def delete_position(position_id: UUID, user: CurrentUserDep, session: SessionDep):
+    position_service = PositionService(session)
+    result = await position_service.delete(position_id, user.id)
+    return result
 
 
 @router.post("/{position_id}/applications")
--- src/routers/v1/project.py
+++ src/routers/v1/project.py
@@ -1,9 +1,12 @@
+from typing import List
 from uuid import UUID
 
 from fastapi import APIRouter
 
 from routers.depenencies import SessionDep, CurrentUserDep
+from schemas.position import PositionCreateSchema, PositionSchema
 from schemas.project import ProjectSchema, ProjectCreateSchema, ProjectUpdateSchema
+from services.position_service import PositionService
 from services.project_service import ProjectService
 
 router = APIRouter(prefix="/projects", tags=["Projects"])
@@ -36,11 +39,15 @@ async def delete(project_id: UUID, user: CurrentUserDep, session: SessionDep):
     return await project_service.delete(project_id, user.id)
 
 
-@router.post("/{project_id}/positions")
-async def add_position(project_id: UUID):
-    ...
+@router.post("/{project_id}/positions", response_model=PositionSchema)
+async def add_position(project_id: UUID, data: PositionCreateSchema, user: CurrentUserDep, session: SessionDep):
+    position_service = PositionService(session)
+    db_obj = await position_service.create(project_id, data, user.id)
+    return PositionSchema.from_orm(db_obj)
 
 
-@router.get("/{project_id}/positions")
-async def get_positions(project_id: UUID):
-    ...
+@router.get("/{project_id}/positions", response_model=List[PositionSchema])
+async def get_positions(project_id: UUID, session: SessionDep):
+    position_service = PositionService(session)
+    db_list = await position_service.get_by_project_id(project_id)
+    return [PositionSchema.from_orm(position) for position in db_list]
