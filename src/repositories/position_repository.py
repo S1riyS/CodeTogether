7158d75e7a00a083b7deda35ae0107d4a86bf4a8
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2024-08-20 21:41:17 +0500

    :sparkles: feat(PositionRepository): added `get_project_owner` method

--- src/repositories/position_repository.py
+++ src/repositories/position_repository.py
@@ -1,9 +1,10 @@
-from typing import List, Sequence
+from typing import List, Sequence, Optional
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
-from models import PositionModel
+from models import PositionModel, UserModel, ProjectModel
 from repositories._base import BaseRepository
 from schemas.position import PositionCreateSchema, PositionUpdateSchema
 from typing_ import IDType
@@ -17,3 +18,15 @@ class PositionRepository(BaseRepository[PositionModel, PositionCreateSchema, Pos
         statement = select(PositionModel).filter_by(project_id=project_id)
         result = await self._session.execute(statement)
         return result.scalars().all()
+
+    async def get_project_owner(self, id_: IDType) -> Optional[UserModel]:
+        result = await self._session.execute(
+            select(PositionModel)
+            .filter_by(id=id_)
+            .options(
+                joinedload(PositionModel.project).
+                joinedload(ProjectModel.owner)
+            )
+        )
+        position = result.scalars().first()
+        return position.project.owner if position else None
